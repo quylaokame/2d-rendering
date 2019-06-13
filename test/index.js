44ba53cb362b652bb965e8603be8980f75606c7a
Tiennguyen <tiennguyen2021@gmail.com>
2019-06-13 21:25:35 +0700

    update test for Scene Graph

--- test/index.js
+++ test/index.js
@@ -1,27 +1,33 @@
 import {Renderer} from "../lib/renderer/Renderer.js";
 import {Loader} from "../lib/resource/Loader.js";
+import {Container} from "../lib/display/Container.js";
 import {Sprite} from "../lib/display/Sprite.js";
 
 
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     var myCanvas = document.getElementById("GameCanvas");
     var renderer = new Renderer(myCanvas);
 
-    var loader = new Loader();
-    loader.onLoaded = function(){
-        init();
-    }
-    loader.addImage('myCar','./assets/mycar.png');
+    init();
 
+    function init() {
 
-    function init(){
-        var textures = loader.atlas.textures;
-        var mycar = new Sprite(renderer, textures['myCar']);
-        renderer.addChild(mycar);
+        var layerItem = new Container(renderer);
+        renderer.addChild(layerItem);
+
+        var mycar = new Sprite(renderer, "./assets/myCar.png");
+        layerItem.addChild(mycar);
         mycar.x = 100;
         mycar.y = 200;
         mycar.width = 50;
         mycar.height = 100;
+
+        var redCar = new Sprite(renderer, "./assets/redCar.png");
+        layerItem.addChild(redCar);
+        redCar.x = 100;
+        redCar.y = 150;
+        redCar.width = 50;
+        redCar.height = 100;
     }
 
 });
