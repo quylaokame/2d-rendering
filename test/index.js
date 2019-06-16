125bf652cbd1c52261395be3a4cc34c43bcce944
tiennguyen <tiennguyen2021@gmail.com>
2019-06-17 05:45:38 +0700

    update test

--- test/index.js
+++ test/index.js
@@ -11,25 +11,38 @@ window.addEventListener("load", function () {
 
     function init() {
 
-        var layerItem = new Container(renderer);
-        renderer.addChild(layerItem);
+        var layerItem = new Container();
+        renderer.screen.addChild(layerItem);
 
         layerItem.x = 20;
         layerItem.y = 20;
 
-        var mycar = new Sprite(renderer, "./assets/myCar.png");
+        var mycar = new Sprite("./assets/myCar.png");
         layerItem.addChild(mycar);
-        mycar.x = 100;
-        mycar.y = 200;
+        mycar.x = 200;
+        mycar.y = 400;
         mycar.width = 50;
         mycar.height = 100;
 
-        var redCar = new Sprite(renderer, "./assets/redCar.png");
+        var redCar = new Sprite("./assets/redCar.png");
         layerItem.addChildAt(redCar,0);
         redCar.x = 100;
         redCar.y = 150;
         redCar.width = 50;
         redCar.height = 100;
+
+        runMyCar();
+        runRedCar();
+
+        function runMyCar(){
+            mycar.y = 400;
+            renderer.tween(mycar, 5000, {y: -100}, runMyCar);
+        }
+
+        function runRedCar(){
+            redCar.y = 400;
+            renderer.tween(redCar, 6000, {y: -100}, runRedCar);
+        }
     }
 
 });
