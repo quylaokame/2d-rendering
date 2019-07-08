6cf2d017950508c9534eb9fd818833faf09f6b61
tiennguyen <tiennguyen2021@gmail.com>
2019-07-08 12:16:35 +0700

    create class RedCar

---
+++ test/game/ui/RedCar.js
@@ -0,0 +1,33 @@
+export class RedCar extends DEMO.Sprite {
+    constructor() {
+        super("./assets/redCar.png");
+        this.width = 50;
+        this.listPosX = [135, 215];
+        this.x = 215;
+        this.y = -100;
+        this.height = 100;
+        this.duration = 5000;
+    }
+
+    run() {
+        gameRenderer.tween(this, this.duration, { y: 400}, () => {
+            this.reset();
+            this.run();
+        })
+    }
+
+    stop(){
+
+    }
+
+    reset(){
+        this.randomPositionX();
+        this.y = -100;
+    }
+
+    randomPositionX(){
+        let index = Math.round(Math.random());
+        this.x = this.listPosX[index];
+    }
+
+}
