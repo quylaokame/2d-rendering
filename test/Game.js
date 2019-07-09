98fce941e5939faea47f03bdea02d0e17d33ecbb
tiennguyen <tiennguyen2021@gmail.com>
2019-07-09 23:52:29 +0700

    simple Game so do not need model

--- test/Game.js
+++ test/Game.js
@@ -1,19 +1,11 @@
-import {GameBoard} from "./game/GameBoard.js";
 import {GameMovie} from "./game/GameMovie.js";
 import {Controller} from "./game/Controller.js";
 
 export class Game {
     constructor() {
-        //model
-        this.gameBoard = new GameBoard();
         //view
         this.gameMovie = new GameMovie();
         //controller
         this.controller = new Controller(this);
-        this.init();
-    }
-
-    init(){
-
     }
 }
