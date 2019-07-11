7cea5e82e78b8705142868ed79ff775a3bd3cd71
Tiennguyen <tiennguyen2021@gmail.com>
2019-07-11 19:43:25 +0700

    update entry

--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ window.addEventListener("load", function () {
     var myCanvas = document.getElementById("GameCanvas");
     window.gameRenderer = new DEMO.Renderer(myCanvas);
     window.game = new Game();
+    game.init();
 
     gameRenderer.screen.addChild(game.gameMovie);
 
