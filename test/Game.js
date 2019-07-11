9de34647c9e9d95d496c1a5b814f8351bbcec274
Tiennguyen <tiennguyen2021@gmail.com>
2019-07-11 19:41:03 +0700

    ad game score

--- test/Game.js
+++ test/Game.js
@@ -1,11 +1,36 @@
 import {GameMovie} from "./game/GameMovie.js";
-import {Controller} from "./game/Controller.js";
 
 export class Game {
     constructor() {
+        this.score = 0;
+    }
+
+    init() {
         //view
         this.gameMovie = new GameMovie();
-        //controller
-        this.controller = new Controller(this);
+
+        this.input = new DEMO.Input();
+
+        this.initEvents();
+    }
+
+    initEvents() {
+        this.input.keydown('ArrowLeft', () => {
+            this.gameMovie.moveLeft();
+        });
+        this.input.keydown('ArrowRight', () => {
+            this.gameMovie.moveRight();
+        });
+        this.input.keydown('Space', () => {
+            this.gameMovie.startGame();
+        });
+    }
+
+    updateScore(newScore) {
+        gameRenderer.tween(this, 300, {score: newScore}, () => {
+            this.gameMovie.txtScore.text = this.score;
+        }, () => {
+            this.gameMovie.txtScore.text = Math.round(this.score).toString();
+        })
     }
 }
